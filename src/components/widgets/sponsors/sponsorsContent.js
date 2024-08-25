2654e8b73ab3abe89e10d3bdf7f739b814222a01
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2024-08-26 00:36:31 +0800

    initialize sponsors content (placeholder)

---
+++ src/components/widgets/sponsors/sponsorsContent.js
@@ -0,0 +1,64 @@
+// listed in order of appearance/importance
+export const sponsorTiers = [
+    {
+        heading: 'Keystone Benefactor',
+        importance: 1,
+        sponsors: [
+            {
+                name: 'Sponsor 1',
+                tagline: 'Sky is the limit',
+                logo: 'https://via.placeholder.com/150',
+                website: 'https://example.com',
+            }
+        ],
+    },
+    {
+        heading: 'Co-Presented by:',
+        importance: 2,
+        sponsors: [
+            {
+                name: 'Sponsor 2',
+                tagline: 'Together we thrive',
+                logo: 'https://via.placeholder.com/150',
+                website: 'https://example.com',
+            },
+            {
+                name: 'Sponsor 3',
+                tagline: 'Empowering communities',
+                logo: 'https://via.placeholder.com/150',
+                website: 'https://example.com',
+            },
+            {
+                name: 'Sponsor 4',
+                tagline: 'Innovation for all',
+                logo: 'https://via.placeholder.com/150',
+                website: 'https://example.com',
+            }
+        ],
+    },
+    {
+        heading: 'Sponsored by:',
+        importance: 3,
+        sponsors: []
+    },
+    {
+        heading: 'In Cooperation With:',
+        importance: 4,
+        sponsors: []
+    }
+    , {
+        heading: 'In Partnership With:',
+        importance: 5,
+        sponsors: []
+    },
+    {
+        heading: 'Contributing Partners',
+        importance: 6,
+        sponsors: []
+    },
+    {
+        heading: 'Community Patners',
+        importance: 6,
+        sponsors: []
+    }
+]
