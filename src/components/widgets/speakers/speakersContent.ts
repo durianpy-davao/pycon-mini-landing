ab339ccc656b260c848160f7f3954306115f144a
44mira <legolaslada@gmail.com>
2024-09-06 21:56:29 +0800

    feat: desktop view base cards

---
+++ src/components/widgets/speakers/speakersContent.ts
@@ -0,0 +1,72 @@
+export type Speaker = {
+  name: string;
+  title: string;
+  position: string;
+  organization: string;
+  talkTitle: string;
+  description: string;
+  image: string;
+};
+
+export const speakersPlaceholder: Speaker[] = [
+  {
+    name: 'Steve Jobs',
+    title: 'AWS Community Builder',
+    position: 'Position',
+    organization: 'Organization',
+    talkTitle: 'Talk Title',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    image: 'https://pngimg.com/uploads/steve_jobs/steve_jobs_PNG33450.png',
+  },
+  {
+    name: 'Steve Jobs',
+    title: 'AWS Community Builder',
+    position: 'Position',
+    organization: 'Organization',
+    talkTitle: 'Talk Title',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    image: 'https://pngimg.com/uploads/steve_jobs/steve_jobs_PNG33450.png',
+  },
+  {
+    name: 'Steve Jobs',
+    title: 'AWS Community Builder',
+    position: 'Position',
+    organization: 'Organization',
+    talkTitle: 'Talk Title',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    image: 'https://pngimg.com/uploads/steve_jobs/steve_jobs_PNG33450.png',
+  },
+  {
+    name: 'Steve Jobs',
+    title: 'AWS Community Builder',
+    position: 'Position',
+    organization: 'Organization',
+    talkTitle: 'Talk Title',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    image: 'https://pngimg.com/uploads/steve_jobs/steve_jobs_PNG33450.png',
+  },
+  {
+    name: 'Steve Jobs',
+    title: 'AWS Community Builder',
+    position: 'Position',
+    organization: 'Organization',
+    talkTitle: 'Talk Title',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    image: 'https://pngimg.com/uploads/steve_jobs/steve_jobs_PNG33450.png',
+  },
+  {
+    name: 'Steve Jobs',
+    title: 'AWS Community Builder',
+    position: 'Position',
+    organization: 'Organization',
+    talkTitle: 'Talk Title',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    image: 'https://pngimg.com/uploads/steve_jobs/steve_jobs_PNG33450.png',
+  },
+];
